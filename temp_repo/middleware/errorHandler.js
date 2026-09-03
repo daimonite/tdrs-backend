db45428b5f0ceeff164e206ccafed8bf00c26c0b
Kelvin Daimon <kelvindaimon2908@gmail.com>
2026-09-03 12:32:24 +0300

    structure changes

--- temp_repo/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (error, req, res, next) => {
-  const status = error.status || 500;
-  const message = error.message || 'Internal server error';
-  console.error(`Error: ${message} (Status: ${status})`); // Log error for debugging
-  res.status(status).json({ error: message });
-
-};
-
-export default errorHandler;
